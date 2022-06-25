1a910014f793ccda417d3cd252f4af864d906013
JohnSmithWithHaruhi <JohnSmithWithHaruhi@users.noreply.github.com>
2022-06-25 19:53:47 +0100

    Changed list item design

--- app/src/main/java/com/example/coronavirus/presentation/view/WeeklyCaseAdapter.kt
+++ app/src/main/java/com/example/coronavirus/presentation/view/WeeklyCaseAdapter.kt
@@ -1,14 +1,15 @@
-package com.example.coronavirus.presentation.viewmodel
+package com.example.coronavirus.presentation.view
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.coronavirus.databinding.WeeklyCaseItemBinding
+import com.example.coronavirus.presentation.viewmodel.WeeklyCase
 
 class WeeklyCaseAdapter(private val dataSet: MutableList<WeeklyCase>) :
     RecyclerView.Adapter<WeeklyCaseAdapter.ViewHolder>() {
 
-    fun updateDateSet(dataSet: List<WeeklyCase>){
+    fun updateDateSet(dataSet: List<WeeklyCase>) {
         this.dataSet.clear()
         this.dataSet.addAll(dataSet)
         notifyDataSetChanged()
@@ -22,7 +23,9 @@ class WeeklyCaseAdapter(private val dataSet: MutableList<WeeklyCase>) :
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         with(viewHolder.binding) {
-            title.text = dataSet[position].date.toString()
+            val data = dataSet[position]
+            date.text = data.date.toString()
+            caseNumber.text = data.cumCases.toString()
         }
     }
 
