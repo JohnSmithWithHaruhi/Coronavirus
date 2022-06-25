54c0cdaa1f22dd1c18af52126c6e4d2985564c78
JohnSmithWithHaruhi <JohnSmithWithHaruhi@users.noreply.github.com>
2022-06-25 20:25:58 +0100

    Connect repository to view model

--- app/src/main/java/com/example/coronavirus/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/coronavirus/presentation/viewmodel/MainViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.coronavirus.presentation.viewmodel
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,7 +11,9 @@ import com.example.coronavirus.data.repository.CovidRepository
 import com.example.coronavirus.data.repository.CovidRepositoryImpl
 import kotlinx.coroutines.launch
 import org.jetbrains.annotations.TestOnly
+import java.time.DayOfWeek.SUNDAY
 import java.time.LocalDate
+import java.time.temporal.TemporalAdjusters.previous
 
 class MainViewModel : ViewModel() {
 
@@ -22,25 +26,46 @@ class MainViewModel : ViewModel() {
 
     fun fetchWeeklyCaseList() {
         viewModelScope.launch {
-            weeklyCaseList.postValue(
-                convertDailyCaseToWeeklyCase(
-                    covidRepository.fetchDailyCaseList()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                weeklyCaseList.postValue(
+                    convertDailyCaseToWeeklyCase(
+                        covidRepository.fetchDailyCaseList()
+                    )
                 )
-            )
+            }
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @TestOnly
     fun convertDailyCaseToWeeklyCase(dailyCaseList: List<DailyCase>): List<WeeklyCase> {
-        return listOf(
-            WeeklyCase(
-                date = LocalDate.now(),
-                cumCases = 123123,
-                dailyNewCase = listOf(1, 2, 3, 4, 5, 6, 7),
-            )
-        )
-    }
+        val weeklyCaseList = mutableListOf<WeeklyCase>()
 
+        var baseDay = LocalDate.now().with(previous(SUNDAY))
+        var cumCases = 0
+        val dailyNewCaseList = mutableListOf<Int>()
+
+
+        dailyCaseList.forEach {
+            if (LocalDate.parse(it.date).toEpochDay() > baseDay.toEpochDay()) {
+                cumCases += it.newCasesByPublishDate
+                dailyNewCaseList.add(it.newCasesByPublishDate)
+            } else {
+                baseDay = baseDay.minusWeeks(1)
+                weeklyCaseList.add(
+                    WeeklyCase(
+                        baseDay,
+                        cumCases,
+                        dailyNewCaseList
+                    )
+                )
+                cumCases = 0
+                dailyNewCaseList.clear()
+            }
+        }
+
+        return weeklyCaseList
+    }
 }
 
 data class WeeklyCase(
