890712d5b3ffba93714ff57011292fa0ab07c4b8
JohnSmithWithHaruhi <JohnSmithWithHaruhi@users.noreply.github.com>
2022-11-22 15:56:24 +0800

    Replace list to compose

---
+++ app/src/main/java/com/example/coronavirus/presentation/ui/WeeklyCaseItem.kt
@@ -0,0 +1,68 @@
+package com.example.coronavirus.presentation.ui
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.coronavirus.R
+
+@Composable
+fun WeeklyCaseItem(
+    date: String, totalCase: String, weeklyCase: String
+) {
+    var isExpand by remember { mutableStateOf(false) }
+    Row(
+        modifier = Modifier
+            .height(64.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column {
+            Text(
+                text = date,
+                style = MaterialTheme.typography.bodySmall,
+            )
+            if (isExpand) {
+                Text(
+                    text = totalCase,
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            } else {
+                Text(
+                    text = weeklyCase, style = MaterialTheme.typography.titleMedium
+                )
+            }
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        IconButton(onClick = { isExpand = !isExpand }) {
+            if (isExpand) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_expand_less_24dp),
+                    contentDescription = ""
+                )
+            } else {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_expand_more_24dp),
+                    contentDescription = ""
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WeeklyCasePreview() {
+    WeeklyCaseItem(
+        date = "Apr 1, 2022", weeklyCase = "1,227,288", totalCase = "Total: 13,117,469"
+    )
+}
--- app/src/main/java/com/example/coronavirus/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/coronavirus/presentation/view/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.coronavirus.presentation.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.coronavirus.R
 import com.example.coronavirus.databinding.ActivityMainBinding
+import com.example.coronavirus.presentation.ui.WeeklyCaseItem
 import com.example.coronavirus.presentation.viewmodel.MainViewModel
+import com.example.coronavirus.presentation.viewmodel.WeeklyCase
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,10 +31,23 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: WeeklyCasesAdapter
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val viewModel: MainViewModel by viewModels()
+
+        setContent {
+            val weeklyCaseList: List<WeeklyCase>? by viewModel.weeklyCaseList().observeAsState()
+
+            Scaffold(modifier = Modifier.fillMaxWidth()) {
+                WeeklyCaseList(weeklyCases = weeklyCaseList ?: listOf())
+            }
+        }
+
+        viewModel.fetchWeeklyCaseList()
+
+        /*val viewModel: MainViewModel by viewModels()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -70,7 +94,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        viewModel.fetchWeeklyCaseList()
+        viewModel.fetchWeeklyCaseList()*/
     }
 
     private fun showReloadView() {
@@ -81,5 +105,20 @@ class MainActivity : AppCompatActivity() {
     private fun hideReloadView() {
         binding.reloadView.visibility = View.INVISIBLE
         binding.recyclerView.visibility = View.VISIBLE
+    }
+
+    @Composable
+    fun WeeklyCaseList(weeklyCases: List<WeeklyCase>) {
+        LazyColumn {
+            weeklyCases.map {
+                item {
+                    WeeklyCaseItem(
+                        date = it.date,
+                        totalCase = it.totalCumCases.toString(),
+                        weeklyCase = it.weeklyCumCases.toString(),
+                    )
+                }
+            }
+        }
     }
 }
