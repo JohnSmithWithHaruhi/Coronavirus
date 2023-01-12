5c55b4be02ec994736b556790d901b26b466d42e
JohnSmithWithHaruhi <JohnSmithWithHaruhi@users.noreply.github.com>
2023-01-12 11:52:48 +0800

    Apply dynamic colour theme

--- app/src/main/java/com/example/coronavirus/MainActivity.kt
+++ app/src/main/java/com/example/coronavirus/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.coronavirus
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.weeklycase.WeeklyCaseScreen
@@ -15,19 +20,27 @@ import dagger.hilt.android.AndroidEntryPoint
  */
 @AndroidEntryPoint
 @OptIn(ExperimentalLifecycleComposeApi::class)
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val viewModel: WeeklyCaseViewModel by viewModels()
 
         setContent {
-            WeeklyCaseScreen(
-                weeklyCaseUiState = viewModel.weeklyCaseUiState.collectAsStateWithLifecycle().value,
-                searchAreaDialogUiState = viewModel.searchAreaDialogUiState.collectAsStateWithLifecycle().value,
-                onReload = viewModel::reload,
-                onSelectSearchArea = viewModel::selectSearchArea,
-            )
+            MaterialTheme(
+                colorScheme = if (isSystemInDarkTheme()) {
+                    dynamicDarkColorScheme(LocalContext.current)
+                } else {
+                    dynamicLightColorScheme(LocalContext.current)
+                }
+            ) {
+                WeeklyCaseScreen(
+                    weeklyCaseUiState = viewModel.weeklyCaseUiState.collectAsStateWithLifecycle().value,
+                    searchAreaDialogUiState = viewModel.searchAreaDialogUiState.collectAsStateWithLifecycle().value,
+                    onReload = viewModel::reload,
+                    onSelectSearchArea = viewModel::selectSearchArea,
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/example/coronavirus/MainApplication.kt
+++ app/src/main/java/com/example/coronavirus/MainApplication.kt
@@ -1,16 +1,10 @@
 package com.example.coronavirus
 
 import android.app.Application
-import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 /**
  * Main application.
  */
 @HiltAndroidApp
-class MainApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        DynamicColors.applyToActivitiesIfAvailable(this)
-    }
-}
+class MainApplication : Application()
